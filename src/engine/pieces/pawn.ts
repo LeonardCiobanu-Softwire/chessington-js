339ae4b4e43007407fef57329cbd0bb6eadcf756
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 15:37:16 +0100

    made pawns able to capture diagonally

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -3,6 +3,7 @@ import Player from '../player';
 import Board from '../board';
 import Square from "../square";
 import GameSettings from "../gameSettings";
+import player from "../player";
 
 export default class Pawn extends Piece {
 
@@ -28,8 +29,36 @@ export default class Pawn extends Piece {
         return null;
     }
 
+    public capture(board: Board): Square[] {
+        let curPos = board.findPiece(this);
+        let moves: Square[] = [];
+        if (this.player === Player.WHITE) {
+            if (board.isSquareOccupied(Square.at(curPos.row + 1, curPos.col - 1))) {
+                let otherPiece = board.getPiece(Square.at(curPos.row + 1, curPos.col - 1));
+                if (otherPiece?.player === Player.BLACK && otherPiece?.pieceType !== 'king')
+                    moves.push(Square.at(curPos.row + 1, curPos.col - 1));
+            }
+            if (board.isSquareOccupied(Square.at(curPos.row + 1, curPos.col + 1))) {
+                let otherPiece = board.getPiece(Square.at(curPos.row + 1, curPos.col + 1));
+                if (otherPiece?.player === Player.BLACK && otherPiece?.pieceType !== 'king')
+                    moves.push(Square.at(curPos.row + 1, curPos.col + 1));
+            }
+        } else if (this.player == Player.BLACK) {
+            if (board.isSquareOccupied(Square.at(curPos.row - 1, curPos.col - 1))) {
+                let otherPiece = board.getPiece(Square.at(curPos.row - 1, curPos.col - 1));
+                if (otherPiece?.player === Player.WHITE && otherPiece?.pieceType !== 'king')
+                    moves.push(Square.at(curPos.row - 1, curPos.col - 1));
+            }
+            if (board.isSquareOccupied(Square.at(curPos.row - 1, curPos.col + 1))) {
+                let otherPiece = board.getPiece(Square.at(curPos.row - 1, curPos.col + 1));
+                if (otherPiece?.player === Player.WHITE && otherPiece?.pieceType !== 'king')
+                    moves.push(Square.at(curPos.row - 1, curPos.col + 1));
+            }
+        }
+        return moves;
+    }
+
     public getAvailableMoves(board: Board) {
-        // return new Array(0);
         let availableMoves: Square[] = [];
         let oneUp: Square | null = this.pawnUp(board);
         if (oneUp)
@@ -38,6 +67,7 @@ export default class Pawn extends Piece {
         if (twoUp)
             availableMoves.push(twoUp);
         availableMoves = availableMoves.filter(move => !board.isSquareOccupied(move) && board.isInBounds(move));
+        availableMoves = availableMoves.concat(this.capture(board));
         return availableMoves;
     }
 }
