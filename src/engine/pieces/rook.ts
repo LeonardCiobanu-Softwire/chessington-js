42463c33c5dc32207a4d7c3813d8eea8d44e38f0
Leonard Ciobanu <Leonard.Ciobanu@softwire.com>
2025-07-07 13:41:48 +0300

    made rook move laterally

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,13 +1,27 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import GameSettings from "../gameSettings";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
+
+
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let currentPosition: Square = board.findPiece(this);
+        let availableMoves: Square[] = [];
+        for (let i: number = 0; i < GameSettings.BOARD_SIZE; i++) {
+            if (i != currentPosition.row) {
+                availableMoves.push(Square.at(i, currentPosition.col));
+            }
+            if (i != currentPosition.col) {
+                availableMoves.push(Square.at(currentPosition.row, i));
+            }
+        }
+        return availableMoves;
     }
 }
