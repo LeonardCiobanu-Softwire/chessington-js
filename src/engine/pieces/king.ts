68eaa9b787f11ddb83fa90aa78b32749aa419c73
Leonard Ciobanu <Leonard.Ciobanu@softwire.com>
2025-07-07 16:59:12 +0300

    made the king do the same

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -23,7 +23,15 @@ export default class King extends Piece {
 
     public getAvailableMoves(board: Board) {
         let availableMoves: Square[] = this.getPossibleMoves(board.findPiece(this));
-        availableMoves = availableMoves.filter(board.isInBounds);
+        availableMoves = availableMoves.filter(move => {
+            if (!board.isSquareOccupied(move))
+                return board.isInBounds(move);
+
+            let blockingPiece: Piece | undefined = board.getPiece(move);
+            let player: boolean = blockingPiece?.player !== this.player;
+            let king: boolean = blockingPiece?.pieceType !== "king";
+            return board.isInBounds(move) && blockingPiece && player && king;
+        });
         return availableMoves;
     }
 }
