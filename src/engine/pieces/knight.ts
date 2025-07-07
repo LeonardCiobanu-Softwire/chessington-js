caf8b732955491298cbc09b61b8914f3d747134c
Leonard Ciobanu <Leonard.Ciobanu@softwire.com>
2025-07-07 14:51:49 +0300

    made knight move in L shapes

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,13 +1,34 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import GameSettings from "../gameSettings";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
         super(player);
     }
+    private getPossibleMoves(currentPosition:Square): Square[] {
+        return [
+        Square.at(currentPosition.row - 1, currentPosition.col + 2),
+        Square.at(currentPosition.row - 1, currentPosition.col - 2),
+        Square.at(currentPosition.row + 1, currentPosition.col + 2),
+        Square.at(currentPosition.row + 1, currentPosition.col - 2),
 
+        Square.at(currentPosition.row + 2, currentPosition.col - 1),
+        Square.at(currentPosition.row - 2, currentPosition.col - 1),
+        Square.at(currentPosition.row + 2, currentPosition.col + 1),
+        Square.at(currentPosition.row - 2, currentPosition.col + 1)];
+    }
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let currentPosition: Square = board.findPiece(this);
+
+        let availableMoves: Square[] = [];
+        for (let s of this.getPossibleMoves(currentPosition)) {
+            if (s.row >= 0 && s.row < GameSettings.BOARD_SIZE && s.col >= 0 && s.col < GameSettings.BOARD_SIZE) {
+                availableMoves.push(s);
+            }
+        }
+        return availableMoves;
     }
 }
